58f4595f4d042abe9230ab3a343f5c12db2d9b55
William Young <fractalwizz@hotmail.com>
2016-03-10 00:29:11 -0600

    Commit #4: GoogleCloudEndpoint Module Added

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,23 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
-import com.udacity.gradle.builditbigger.joker.Joker;
 import com.fract.nano.williamyoung.jokedisplay.JokeDisplay;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 public class MainActivity extends ActionBarActivity {
     @Override
@@ -27,18 +35,41 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-//        if (id == R.id.action_settings) { return true; }
-//
-//        return super.onOptionsItemSelected(item);
         return (id == R.id.action_settings) || super.onOptionsItemSelected(item);
     }
 
     public void tellJoke(View view) {
-        Joker joker = new Joker();
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "joke"));
+    }
 
-        Intent intent = new Intent(this, JokeDisplay.class);
-        intent.putExtra("joke", joker.getJoke());
-        startActivity(intent);
+    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private MyApi apiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if (apiService == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://jokesbigger.appspot.com/_ah/api/");
+
+                apiService = builder.build();
+            }
+
+            context = params[0].first;
+            String joke = params[0].second;
+
+            try {
+                return apiService.sayJoke(joke).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(getApplicationContext(), JokeDisplay.class);
+            intent.putExtra("joke", result);
+            startActivity(intent);
+        }
     }
 }
